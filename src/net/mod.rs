48c2a189dc11d95cfda7def17de92741842a9e07
Y. T. Chung <zonyitoo@gmail.com>
2015-06-16 23:59:58 +0800

    export shutdown

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -19,7 +19,7 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-pub use self::tcp::{TcpListener, TcpStream, TcpSocket};
+pub use self::tcp::{TcpListener, TcpStream, TcpSocket, Shutdown};
 pub use self::udp::UdpSocket;
 
 use std::io;
