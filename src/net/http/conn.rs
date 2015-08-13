c12cbb4f1262b40d8870572d2d7aa19657141ac9
Y. T. Chung <zonyitoo@gmail.com>
2015-08-13 13:33:00 +0800

    allow deadcode, fix git code version

--- src/net/http/conn.rs
+++ src/net/http/conn.rs
@@ -20,6 +20,8 @@
 //  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 //  DEALINGS IN THE SOFTWARE.
 
+#![allow(dead_code)]
+
 use std::io::{self, Read, Write};
 use std::net::{ToSocketAddrs, SocketAddr, Shutdown};
 use std::fmt;
